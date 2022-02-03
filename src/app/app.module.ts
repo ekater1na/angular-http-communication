5a78eab0345435b808d45d1b189bce5ccb99d551
ekater1na <ekaterina_l@tut.by>
2022-02-03 20:16:45 +0300

    feat: add interceptor to cache http request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import {BookTrackerErrorHandlerService} from './core/book-tracker-error-handler.
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {AddHeaderInterceptor} from './core/add-header.interceptor';
 import {LogResponseInterceptor} from './core/log-response.interceptor';
+import {CacheInterceptor} from './core/cache.interceptor';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import {LogResponseInterceptor} from './core/log-response.interceptor';
   providers: [
     {provide: ErrorHandler, useClass: BookTrackerErrorHandlerService},
     {provide: HTTP_INTERCEPTORS, useClass: LogResponseInterceptor, multi: true},
-    {provide: HTTP_INTERCEPTORS, useClass: AddHeaderInterceptor, multi: true}
+    {provide: HTTP_INTERCEPTORS, useClass: AddHeaderInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: CacheInterceptor, multi: true}
   ],
   imports: [
     BrowserModule,
